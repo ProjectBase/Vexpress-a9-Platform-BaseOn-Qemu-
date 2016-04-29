4f8bcdcd72dce1f927fd97003b3af97a83531764
mobo <mob5szh@g3gdev-mob5szh-2.(none)>
2016-04-29 13:04:24 +0200

    works,but binaries in /bin folder not found. maybe some configure need to coorect

---
+++ rootfs/share/gdb/auto-load/libglib-2.0.so.0.4600.2-gdb.py
@@ -0,0 +1,10 @@
+import sys
+import gdb
+
+# Update module path.
+dir_ = '/usr/OSS/glib_installglib_cv_long_long_format=yes/share/glib-2.0/gdb'
+if not dir_ in sys.path:
+    sys.path.insert(0, dir_)
+
+from glib import register
+register (gdb.current_objfile ())
