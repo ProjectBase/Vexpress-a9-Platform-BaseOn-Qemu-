88af82f14bf346f8f87db35a3d6a8129fd7613b7
mobo <mob5szh@g3gdev-mob5szh-2.(none)>
2016-05-06 10:50:26 +0200

    add modules config

--- mkimage.sh
+++ mkimage.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-cd /usr/src/linux-3.14
-make CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm vexpress_defconfig
+cd /usr/src/linux-3.14.48
+#make CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm vexpress_defconfig
 make CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm
+make CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm  modules_install INSTALL_MOD_PATH=/usr/M_Workspace/vexpress-a9/rootfs
+
+
