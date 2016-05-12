9ecc5faadaae8356483424ddb3f305bc3603a6bb
mobo <mob5szh@g3gdev-mob5szh-2.(none)>
2016-05-12 04:50:09 +0200

    add openssh to support ssh telnet

--- host_config/config_net.sh
+++ host_config/config_net.sh
@@ -4,3 +4,8 @@ sudo echo "#! /bin/bash " > /etc/qemu-ifup
 sudo echo "ifconfig $1 172.20.0.1" >> /etc/qemu-ifup
 sudo chomd +x /etc/qemu-ifup
 
+
+sudo echo "#! /bin/bash " > /etc/qemu-ifdown
+sudo echo "ifconfig $1 172.20.0.1" >> /etc/qemu-ifdown
+sudo chomd +x /etc/qemu-ifdown
+
